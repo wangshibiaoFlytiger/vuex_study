e4602a37203ac376f3e22265177a592a42757bf8
wangshibiao <645102170@qq.com>
2019-12-23 15:09:42 +0800

    feat: 集成vuex,演示vuex的用法

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Parent></Parent>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Parent from "./views/Parent";
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Parent,
   }
 }
 </script>
---
+++ src/views/Parent.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="parent">
+        <h3>这里是父组件</h3>
+        <button type="button" @click="clickHandler">修改自己文本</button>
+        <button type="button" @click="clickHandler2">修改子组件文本</button>
+        <div>Test: {{msg}}</div>
+        <child></child>
+    </div>
+</template>
+
+<script>
+    import store from '../vuex'
+    import Child from './Child.vue'
+
+    export default {
+        name: "Parent",
+        computed: {
+            msg(){
+                return store.state.testMsg;
+            }
+        },
+        methods:{
+            clickHandler(){
+                store.commit('changeTestMsg', '父组件修改自己后的文本')
+            },
+            clickHandler2(){
+                store.commit('changeChildText', '父组件修改子组件后的文本')
+            }
+        },
+        components:{
+            'child': Child
+        },
+        store,
+    }
+</script>
+
+<style scoped>
+    .parent{
+        background-color: #00BBFF;
+        height: 400px;
+    }
+</style>
