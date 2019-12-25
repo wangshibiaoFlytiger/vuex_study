b4431a5f728b06107f497bf3985072d79d64ffd8
wangshibiao <645102170@qq.com>
2019-12-25 17:31:10 +0800

    refactor: 删除冗余代码Vue.prototype.store = store

--- src/main.js
+++ src/main.js
@@ -2,13 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from "./store/index";
 
-// 设置全局变量, 任何组件内都可以通过this.store引用该全局变量
-Vue.prototype.store = store
-
 Vue.config.productionTip = false
 
 new Vue({
-  // 将store定义为vue实例的选项, 则所有组件会全局自动注入该选项
+  //定义全局变量, 任何组件内都可以通过this.$store引用该全局变量.注意引用全局变量时, 需带上前缀$
+  //等价于Vue.prototype.$store = store
   store,
+
   render: h => h(App),
 }).$mount('#app')
