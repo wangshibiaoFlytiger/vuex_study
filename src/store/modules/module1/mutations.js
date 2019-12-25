3f7bbe9cda8e49dd6c384267c964e177370da264
wangshibiao <645102170@qq.com>
2019-12-25 13:01:23 +0800

    refactor: 使用常量替代Mutation的事件类型
    大型项目中推荐采用这种定义方式

--- src/store/modules/module1/mutations.js
+++ src/store/modules/module1/mutations.js
@@ -1,8 +1,21 @@
+import {CHANGE_CHILD_TEXT, CHANGE_TEST_MSG} from "../../mutation-types";
+
 export default {
-    changeTestMsg(state, str){
+    /**
+     * 修改父组件的内容
+     * @param state
+     * @param str
+     */
+    [CHANGE_TEST_MSG](state, str){
         state.testMsg = str;
     },
-    changeChildText(state, str){
+
+    /**
+     * 修改子组件的内容
+     * @param state
+     * @param str
+     */
+    [CHANGE_CHILD_TEXT](state, str){
         state.childText = str;
     }
 };
